5d4bc19df9afcf938b7070646e07f2329b1143bc
Abdumalik Sattorov <abdumalikfinance@gmail.com>
2026-05-18 18:37:13 +0500

    yangi faylar qoshildi
    8  9 10 11 darslar yuklandi

---
+++ git.py
@@ -0,0 +1,17 @@
+def oraliq(min, max, qadam=1):
+    sonlar = []
+    hozirgi = min
+    
+    # Agar qadam 0 bo'lsa, xatolikni oldini olish kerak
+    if qadam == 0:
+        return "Qadam 0 ga teng bo'lishi mumkin emas."
+
+    while hozirgi < max:
+        sonlar.append(hozirgi)
+        hozirgi += qadam  # Har safar qadam miqdoricha oshiramiz
+        
+    return sonlar
+
+# Tekshirish:
+print(oraliq(2, 10, 2))  # Natija: [2, 4, 6, 8]
+print(oraliq(5, 15))     # Natija: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
